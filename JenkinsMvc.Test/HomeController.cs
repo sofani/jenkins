51cbe71df17f28ddf894123a8be66920a15ac3bb
sofani <sofanib@gmail.com>
2018-01-05 10:21:29 -0500

    updated files

--- JenkinsMvc.Test/HomeController.cs
+++ JenkinsMvc.Test/HomeController.cs
@@ -1,14 +1,15 @@
 using Xunit;
 
 namespace JenkinsMvc.Test
-{
-    public class HomeController {
-        [Fact]   
-        public void HelloTest() {
+{ 
+     
+     public class HomeController {
+       
+       
            [Fact]   
            public void HelloTest() {
-
+               var hc  = new HomeController();
+               Assert.NotNull(hc);
            }
         }
     }
-}
