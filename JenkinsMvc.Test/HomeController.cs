f7a07197350e9f3ccb966a2bf7c7d8442260491c
sofani <sofanib@gmail.com>
2018-01-05 10:17:53 -0500

    updated for test purpose

---
+++ JenkinsMvc.Test/HomeController.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace JenkinsMvc.Test
+{
+    public class HomeController {
+        [Fact]   
+        public void HelloTest() {
+           [Fact]   
+           public void HelloTest() {
+
+           }
+        }
+    }
+}
